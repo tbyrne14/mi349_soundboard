236e746c6e5341b9a52040e6af17784fec52edc8
Tyler Byrne <byrnetyl@msu.edu>
2022-11-20 16:36:07 -0500

    starting out

---
+++ soundboard.js
@@ -0,0 +1,9 @@
+const sounds = ['boing', 'cash register', 'cheering']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound 
+    document.getElementById('buttons').appendChild(btn)
+})
